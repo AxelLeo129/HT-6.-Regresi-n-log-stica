70b3cc91d393873a0e5bc7a0a856ce70f036f2f6
Pablo Josue Noack Cajbon <noa17596@uvg.edu.gt>
2021-04-14 23:01:05 -0600

    Regresión lógistica solucionada
    Se solucionaron problemas referentes a los modelos y un sobre ajuste que se tenía entre los datos

--- Regresion_logistica.R
+++ Regresion_logistica.R
@@ -1,24 +1,26 @@
-<<<<<<< HEAD
-setwd("C:/Users/Kevin Macario/Desktop/Uvg/9no Semestre/Mineria de Datos/HT-6.-Regresi-n-log-stica/house-prices-advanced-regression-techniques")
-datatest <- read.csv('test.csv')
-datatrain <- read.csv('train.csv')
-prices <- read.csv('sample_submission.csv')
-=======
+#<<<<<<< HEAD
+#setwd("C:/Users/Kevin Macario/Desktop/Uvg/9no Semestre/Mineria de Datos/HT-6.-Regresi-n-log-stica/house-prices-advanced-regression-techniques")
+#datatest <- read.csv('test.csv')
+#datatrain <- read.csv('train.csv')
+#prices <- read.csv('sample_submission.csv')
+#=======
 library(caret)
 library(dummies)
-setwd("D:/AxelFolder/University/mineria_de_datos/HT-6.-Regresi-n-log-stica")
->>>>>>> c524c2669884e3fb364a8e078a53e10658a44119
+#setwd("D:/AxelFolder/University/mineria_de_datos/HT-6.-Regresi-n-log-stica")
+#>>>>>>> c524c2669884e3fb364a8e078a53e10658a44119
+
+setwd("C:/Users/LENOVO/Desktop/Clases/Minería de datos/Github/HT-6.-Regresi-n-log-stica")
 
 porcentaje<-0.8
 set.seed(666)
 
-<<<<<<< HEAD
-=======
+#<<<<<<< HEAD
+#=======
 datatest <- read.csv("house-prices-advanced-regression-techniques/test.csv")
 prices <- read.csv("house-prices-advanced-regression-techniques/sample_submission.csv")
 datatrain <- read.csv("house-prices-advanced-regression-techniques/train.csv")
 
->>>>>>> c524c2669884e3fb364a8e078a53e10658a44119
+#>>>>>>> c524c2669884e3fb364a8e078a53e10658a44119
 datatestc = merge(x = datatest, y = prices, by = "Id")
 
 
@@ -59,15 +61,30 @@ library(caret)
 library(dummies)
 
 datos<-cbind(datos,dummy(datos$grupo), verbose = T)
-
+mask <- unlist(lapply(datos, is.numeric))
 corte <- sample(nrow(datos),nrow(datos)*porcentaje)
 train<-datos[corte,mask]
 test<-datos[-corte,mask]
 
 ################# 
-
-model<-glm(as.factor(train$datos1)~., data = train[,c(1:38,8)],family = binomial(), maxit=100)
-
-pred<-predict(model,newdata = test[1:38,], type = "response")
+c(1:38,40)
+t <- proc.time()
+model<-glm(as.factor(train$datos1)~., data = train[,c(1:38,40)],family = binomial(), maxit=100)
+pred<-predict(model,newdata = test[,1:38], type = "response")
 prediccion<-ifelse(pred>=0.5,1,0)
-confusionMatrix(as.factor(test$datosvirginica),as.factor(prediccion))
+confusionMatrix(as.factor(test$datos1),as.factor(prediccion))
+proc.time()-t
+
+t <- proc.time()
+model<-glm(as.factor(train$datos2)~., data = train[,c(1:38,41)],family = binomial(), maxit=100)
+pred<-predict(model,newdata = test[,1:38], type = "response")
+prediccion<-ifelse(pred>=0.5,1,0)
+confusionMatrix(as.factor(test$datos2),as.factor(prediccion))
+proc.time()-t
+
+t <- proc.time()
+model<-glm(as.factor(train$datos2)~., data = train[,c(1:38,42)],family = binomial(), maxit=100)
+pred<-predict(model,newdata = test[,1:38], type = "response")
+prediccion<-ifelse(pred>=0.5,1,0)
+confusionMatrix(as.factor(test$datos3),as.factor(prediccion))
+proc.time()-t
