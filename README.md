9cfd227297792ed03f0f3c49d01d7773a42022c0
Pablo Josue Noack Cajbon <noa17596@uvg.edu.gt>
2021-04-22 12:42:20 -0600

    svm

--- README.md
+++
@@ -1,7 +0,0 @@
-# HT 6. Regresión logística
-Minería de Datos sección 10  
-Grupo # 8  
-Autores:  
-- Pablo Noack 17596
-- Axel López 20768
-- Kevin Macario 1736
--- svm.R
+++ svm.R
@@ -6,18 +6,22 @@
 #=======
 library(caret)
 library(dummies)
-setwd("D:/AxelFolder/University/mineria_de_datos/HT-6.-Regresi-n-log-stica")
+library(e1071)
+#setwd("D:/AxelFolder/University/mineria_de_datos/HT-6.-Regresi-n-log-stica")
 #>>>>>>> c524c2669884e3fb364a8e078a53e10658a44119
 
-#setwd("C:/Users/LENOVO/Desktop/Clases/Minería de datos/Github/HT-6.-Regresi-n-log-stica")
+setwd("C:/Users/LENOVO/Desktop/Clases/Minería de datos/Github/HT7SVM")
 
 porcentaje<-0.8
 set.seed(666)
 
+#<<<<<<< HEAD
+#=======
 datatest <- read.csv("house-prices-advanced-regression-techniques/test.csv")
 prices <- read.csv("house-prices-advanced-regression-techniques/sample_submission.csv")
 datatrain <- read.csv("house-prices-advanced-regression-techniques/train.csv")
 
+#>>>>>>> c524c2669884e3fb364a8e078a53e10658a44119
 datatestc = merge(x = datatest, y = prices, by = "Id")
 
 
@@ -54,7 +58,10 @@ plot(histo3, c = c3,add = TRUE)
 
 ###################
 
-datos<-cbind(datos,dummy(datos$grupo), verbose = T)
+library(caret)
+library(dummies)
+
+#datos<-cbind(datos,dummy(datos$grupo), verbose = T)
 mask <- unlist(lapply(datos, is.numeric))
 corte <- sample(nrow(datos),nrow(datos)*porcentaje)
 train<-datos[corte,mask]
@@ -62,44 +69,16 @@ test<-datos[-corte,mask]
 
 ################# 
 
-t <- proc.time()
-model<-glm(as.factor(train$datos1)~., data = train[,c(1:38,40)],family = binomial(), maxit=100)
-pred<-predict(model,newdata = test[,1:38], type = "response")
-prediccion<-ifelse(pred>=0.5,1,0)
-confusionMatrix(as.factor(test$datos1),as.factor(prediccion))
-proc.time()-t
-
-t <- proc.time()
-pred<-predict(model,newdata = train[,1:38], type = "response")
-prediccion<-ifelse(pred>=0.5,1,0)
-confusionMatrix(as.factor(train$datos1),as.factor(prediccion))
-proc.time()-t
+modeloTuneado<-tune.svm(as.factor(grupo)~., data=train, cost=c(0.01,0.1,0.5,1,5), kernel="linear")
 
 
-t <- proc.time()
-model<-glm(as.factor(train$datos2)~., data = train[,c(1:38,41)],family = binomial(), maxit=100)
-pred<-predict(model,newdata = test[,1:38], type = "response")
-prediccion<-ifelse(pred>=0.5,1,0)
-confusionMatrix(as.factor(test$datos2),as.factor(prediccion))
-proc.time()-t
-
-t <- proc.time()
-pred<-predict(model,newdata = train[,1:38], type = "response")
-prediccion<-ifelse(pred>=0.5,1,0)
-confusionMatrix(as.factor(train$datos2),as.factor(prediccion))
-proc.time()-t
+modeloTuneado$performances
+modeloTuneado$best.model
 
 
-t <- proc.time()
-model<-glm(as.factor(train$datos3)~., data = train[,c(1:38,42)],family = binomial(), maxit=100)
-pred<-predict(model,newdata = test[,1:38], type = "response")
-prediccion<-ifelse(pred>=0.5,1,0)
-confusionMatrix(as.factor(test$datos3),as.factor(prediccion))
-proc.time()-t
+pred<-predict(modeloTuneado$best.model,newdata = test[,1:38], type = "response")
+confusionMatrix(as.factor(test$grupo),as.factor(pred))
 
-t <- proc.time()
-pred<-predict(model,newdata = train[,1:38], type = "response")
-prediccion<-ifelse(pred>=0.5,1,0)
-confusionMatrix(as.factor(train$datos3),as.factor(prediccion))
-proc.time()-t
+
+
 
